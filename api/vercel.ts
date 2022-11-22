4bc91c3f11cee98d2033d5defee314cd8e05a4a1
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 23:12:36 +0700

    2022-11-22 23:12

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+// import { join } from 'path';
 import pug from 'pug';
 import { AppModule } from '../src/app.module';
 
@@ -12,7 +12,7 @@ async function bootstrap() {
   // app.setBaseViewsDir(join(__dirname, '..', 'views'));
   // app.setViewEngine('pug');
   app.set('view engine', 'pug');
-  app.set('views', join(__dirname, 'views'));
+  app.set('views', './views');
 
   await app.listen(3000);
   return app;
