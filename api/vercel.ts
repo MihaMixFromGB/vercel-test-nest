851f6ace375ac9df110c208b7d415686544ab30b
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 22:57:59 +0700

    vercel folder move to root

--- api/vercel.ts
+++ api/vercel.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import pug from 'pug';
-import { AppModule } from '../app.module';
+import { AppModule } from '../src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.engine('pug', pug.__express);
-  app.setBaseViewsDir(join(__dirname, '..', '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
   await app.listen(3000);
