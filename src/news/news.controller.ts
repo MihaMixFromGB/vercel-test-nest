a3a213e34b31b17c3c8611e4d65e967a7812cb4d
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 23:01:19 +0700

    2022-11-22 23:01

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller('news')
 export class NewsController {
   @Get()
-  @Render('index')
+  @Render('index.pug')
   async get() {
     return;
   }
