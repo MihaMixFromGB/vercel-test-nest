300d6bf7d66a48951725f96dabb1e4ceafa24216
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 22:33:14 +0700

    2022-11-22 22:33

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.useStaticAssets(join(__dirname, '..', 'views'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
