d8fce9dd45df1cc228da6c659cb9a068aa1b2a90
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 22:49:27 +0700

    2022-11-22 22:49

--- src/api/vercel.ts
+++ src/api/vercel.ts
@@ -7,9 +7,9 @@ import { AppModule } from '../app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', '..', 'public'));
   app.engine('pug', pug.__express);
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '...', '..', 'views'));
   app.setViewEngine('pug');
 
   await app.listen(3000);
