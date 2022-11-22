6efc1bb806dec09a51f47beb57305d5dac1622bb
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 23:57:42 +0700

    2022-11-22 23:57

--- api/vercel.ts
+++ api/vercel.ts
@@ -4,8 +4,6 @@ import { join } from 'path';
 import pug from 'pug';
 import { AppModule } from '../src/app.module';
 
-import '../views';
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
