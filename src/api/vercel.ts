e91ebae06236416ecb1e651c09ffff9e15a52f75
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 22:06:32 +0700

    default start

--- src/api/vercel.ts
+++ src/api/vercel.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
-  // await app.listen(3000);
+  await app.listen(3000);
   return app;
 }
 // bootstrap();
