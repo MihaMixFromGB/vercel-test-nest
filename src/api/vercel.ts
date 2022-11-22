f160f51c162c35d0e6175f6e67487884ecae55fa
MihaMixFromGB <mihamix@yandex.ru>
2022-11-22 22:41:18 +0700

    add dynamic import pug

--- src/api/vercel.ts
+++ src/api/vercel.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { AppModule } from '../app.module';
 import { pug } from 'pug';
+import { AppModule } from '../app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.engine('pug', pug.__express);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
