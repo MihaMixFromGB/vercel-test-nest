a0e44137cfc55801dc8ffad310d681668c3850ea
MihaMixFromGB <mihamix@yandex.ru>
2022-11-23 22:13:16 +0700

    2022-11-23 22:13

--- api/vercel.ts
+++ api/vercel.ts
@@ -3,14 +3,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import pug from 'pug';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import swaggerUi from 'swagger-ui-express';
 import { AppModule } from '../src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'assets/swagger-ui-dist/'), {
-    prefix: '/api',
-  });
+  // app.useStaticAssets(join(__dirname, '..', 'assets/swagger-ui-dist/'), {
+  //   prefix: '/api',
+  // });
   app.engine('pug', pug.__express);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
@@ -22,7 +23,8 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // SwaggerModule.setup('api', app, document);
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(document));
 
   await app.listen(3000);
   return app;
