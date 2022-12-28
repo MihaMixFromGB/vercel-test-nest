757d870087358ddf3e180a5e7c573172b3c8d0f1
MihaMixFromGB <mihamix@yandex.ru>
2022-12-28 11:29:30 +0700

    2022-12-28 11:29

---
+++ api/vercel.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import pug from 'pug';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import swaggerUi from 'swagger-ui-express';
+import { AppModule } from '../src/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'assets/swagger-ui-dist/'), {
+    prefix: '/api',
+  });
+  app.engine('pug', pug.__express);
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('pug');
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example ')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // [GET] /api/api/swagger-ui-init.js
+  // This file isn't in 'swagger-ui-dist' folder
+  // If copy all folder to assets render default api page (Pet Store)
+  // SwaggerModule.setup('api', app, document);
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(document));
+
+  await app.listen(3000);
+  return app;
+}
+// bootstrap();
+module.exports = bootstrap();
